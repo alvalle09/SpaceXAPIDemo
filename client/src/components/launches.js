86acf4efbdc0aea0022308f7d429a365a49350f6
Alfredo Valle <phredvalle@gmail.com>
2022-05-13 00:29:21 -0500

    render LaunchItem component with launches data

--- client/src/components/launches.js
+++ client/src/components/launches.js
@@ -19,21 +19,16 @@ export default function Launches() {
 
     if (error) return <h4>Error!</h4>
     if (loading) return <h4>Loading...</h4>
-
-
+    console.log(data);
+    
     return (        
       <Fragment>
          <h1 className="display-4 my-3">Launches</h1>
-         <h4> Test! </h4>
          {
              data.launches.map(launch => (
                 <LaunchItem key={launch.flight_number} launch={launch} />
              ))
-         }
-
-         
-      </Fragment>
-      
-    )
-    
+         }         
+      </Fragment>      
+    )    
 }
