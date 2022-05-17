53230879b54a8ed795e3e6ec58541c6d474b414b
Alfredo Valle <phredvalle@gmail.com>
2022-05-16 23:35:05 -0500

    testing routing, debugged paths issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Launches from './components/Launches';
-import './App.css';
-import logo from './logo.png';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Launches from "./components/Launches";
+import Launch from './components/Launch';
+import "./App.css";
+import logo from "./logo.png";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache()
-})
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
 
 class App extends Component {
-  render () {
+  render() {
     return (
       <ApolloProvider client={client}>
-        
-        <div className="container">
-          <img 
-            src={logo}
-            alt='SpaceX'
-            style={{ width: 300, display: 'block', margin: 'auto'}}
+        <BrowserRouter>
+          <div className="container">
+            <img
+              src={logo}
+              alt="SpaceX"
+              style={{ width: 300, display: "block", margin: "auto" }}
             />
-            <BrowserRouter>
-        
-              <Routes>
-            
-                <Route exact path='/' element={<Launches/>}/>
-              </Routes>
-              </BrowserRouter>
-        </div>
-        
-      </ApolloProvider>      
+
+            <Routes>
+              <Route exact path="/" element={<Launches />} />
+              <Route exact path="/launch/:flight_number" element={<Launch/>} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </ApolloProvider>
     );
   }
 }
 
 export default App;
- 
