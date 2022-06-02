599eb8133a3a4a57360d57e2046617b622cc0dff
Alfredo Valle <phredvalle@gmail.com>
2022-06-01 23:52:49 -0500

    setup Launch details graphql query

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -1,4 +1,23 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+
+const LAUNCH_QUERY = gql`
+  query LaunchQuery($flight_number: Int!){
+    launch(flight_number: $flight_number) {
+      flight_number
+      mission_name
+      launch_year
+      launch_success
+      launch_date_local
+      rocket {
+        rocket_id
+        rocket_name
+        rocket_type
+      }
+    }
+  }
+`;
 
 export class Launch extends Component {
   render() {
