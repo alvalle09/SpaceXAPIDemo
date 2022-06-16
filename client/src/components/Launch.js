9bd5e36c2d391743ea853a1fb6d89ebb68de1417
Alfredo Valle <phredvalle@gmail.com>
2022-06-16 00:31:48 -0500

    fix bug on flight_number redeclared

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -33,12 +33,31 @@ export default function Launch() {
   if (error) console.log(error);
   console.log(data);
 
+  const {
+    mission_name,
+    launch_year,
+    launch_success,
+    rocket: { rocket_id, rocket_name, rocket_type },
+  } = data.launch;
+
   return (
     <Fragment>
       <div>
         <h1 className="display-4 my-3">
-          <span className="text-dark">Mission:</span> {data.launch.misson_name}
+          <span className="text-dark">Mission:</span>
+          {mission_name}
         </h1>
+        <h4 className="mb-3">Launch Details</h4>
+        <ul className="list-group">
+          <li className="list-group-item">Flight Number: {flight_number}</li>
+          <li className="list-group-item">Launch Year: {launch_year}</li>
+          <li className="list-group-item">
+            Launch Successful: {launch_success ? "Y" : "N"}
+          </li>
+          <li className="list-group-item">
+            Rocket Name: {rocket_name}
+          </li>
+        </ul>
       </div>
     </Fragment>
   );
