4a70fb045b3d1aeb9f9baed7f1dea88cf4811af0
Alfredo Valle <phredvalle@gmail.com>
2022-06-15 23:51:38 -0500

    format code

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -1,11 +1,11 @@
-import React, { Component, Fragment } from 'react'
-import { useQuery } from '@apollo/client';
-import gql from 'graphql-tag';
-import classNames from 'classnames';
-import { Link, useParams } from 'react-router-dom';
+import React, { Component, Fragment } from "react";
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+import classNames from "classnames";
+import { Link, useParams } from "react-router-dom";
 
 const LAUNCH_QUERY = gql`
-  query LaunchQuery($flight_number: Int!){
+  query LaunchQuery($flight_number: Int!) {
     launch(flight_number: $flight_number) {
       flight_number
       mission_name
@@ -21,21 +21,21 @@ const LAUNCH_QUERY = gql`
   }
 `;
 
-export default function Launch () {      
-    let { flight_number } = useParams();
-    flight_number = parseInt(flight_number);
+export default function Launch() {
+  let { flight_number } = useParams();
+  flight_number = parseInt(flight_number);
 
-    const { loading, error, data } = useQuery(LAUNCH_QUERY, {variables: {flight_number}});
+  const { loading, error, data } = useQuery(LAUNCH_QUERY, {
+    variables: { flight_number },
+  });
 
-    if (loading) return  <h4>Loading...</h4> ;
+  if (loading) return <h4>Loading...</h4>;
   if (error) console.log(error);
-   console.log(data);
+  console.log(data);
 
-    return (
-      <Fragment>
-        <h1>Test</h1>
-                
-      </Fragment>
-    )
-  }
-
+  return (
+    <Fragment>
+      <h1>Test</h1>
+    </Fragment>
+  );
+}
