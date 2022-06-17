e81ea7c60e9347bd4554c23c2b725a98841207c2
Alfredo Valle <phredvalle@gmail.com>
2022-06-16 23:36:51 -0500

    fix margen on span

--- client/src/components/MissionKey.js
+++ client/src/components/MissionKey.js
@@ -2,15 +2,15 @@ import React, { Fragment } from "react";
 
 export default function MissionKey() {
   return (
-    <Fragment>    
+    <Fragment>
       <div className="my-3">
-      <p>
-        <span className="px-3 mr-10 bg-success"/> =Success
-      </p>
-      <p>
-        <span className="px-3 mr-10 bg-danger"/> =Failed
-      </p>
-    </div>
+        <p>
+          <span className="px-3 mr-10 bg-success" /> =Success
+        </p>
+        <p>
+          <span className="px-3 mr-10 bg-danger" /> =Failed
+        </p>
+      </div>
     </Fragment>
   );
 }
